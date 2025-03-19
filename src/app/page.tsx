d17d4f66676ab9d8df0e410683a4ff458094eba2
jhowgama <jonathan.gama@teltecsolutions.com.br>
2025-03-19 19:48:15 -0400

    att alias and config ui of shadcn in ds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from '@ds/components/global/mode-toggle';
+
 export default function Home() {
     return (
         <div>
             <h1>Ola Mundo!</h1>
+            <ModeToggle />
         </div>
     );
 }
